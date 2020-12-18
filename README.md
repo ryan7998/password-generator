b91759c34446e7376e5bf5fe645f0c0344ff4b43
fazle <ryan7998@gmail.com>
2020-12-18 17:37:28 -0500

    readme added and error message generation developed

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # password-generator
+# Screenshot:
+![](assets/screenshot.png)
+
+## Password generator developed with javascript.
+
+## Website will ask to input the length of the password and then ask either the password will contain lowercase, uppercase, numeric value or / and special characters through confirm window.
+
+## If length is lower than 8 or greater than 128 the program will send a alert message and also display the message in the website.
+
+## variable charSet will add values depending on the type chosen
+
+## If none of the following: lowercase, uppercase, numeric value or / and special characters, is chosen, the program will give an alert box and also display message that you must choose any of the values to generate password.
--- script.js
+++ script.js
@@ -3,47 +3,51 @@
 function generatePassword(){
   
   // Declare variables:
-  var lowerCase, upperCase, numericValue, spCharacter;
   var length = prompt("Length of password");
 
   // Check if input is between 8 to 128:
   if(length >= 8 && length <= 128){
 
+    // Declare variables;
+    var lowerCase, upperCase, numericValue, spCharacter;
     // Declare empty String charSet to contain password set:
     var charSet = "";
 
-    //Promt user either to select lowercase or not.
-    //If Lower case is selected, add lowercase alphabets to the variable charSet
+    // Promt user either to select lowercase or not.
+    // If Lower case is selected, add lowercase alphabets to the variable charSet
     // else Charset remains empty: 
     if(lowerCase = window.confirm("Do you  want the password to contain lowercase")){
       charSet = "abcdefghijklmnopqrstuvwxyz";
     }
-    //If Upper Case is selected, add uppercase alphabets to the charSet:
+    // If Upper Case is selected, add uppercase alphabets to the charSet:
     if(upperCase = window.confirm("Do you  want the password to contain UPPERCASE")){
       charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
-    //If Numeric Calue is selcted, add numbers to charSet:
+    // If Numeric Calue is selcted, add numbers to charSet:
     if(numericValue = window.confirm("Do you  want the password to contain Numeric Value")){
       charSet += "0123456789";
     }
-    //If special Characters are selected, add those to the charSet:
+    // If special Characters are selected, add those to the charSet:
     // Double quote concatenated with \:
     if(spCharacter = window.confirm("Do you  want the password to contain Special Character")){
       charSet += "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
     }
 
-    //If None is selected alert users to select atleast one type:
+    // If None is selected alert users to select atleast one type:
     if(!lowerCase && !upperCase && !numericValue && !spCharacter){
+      // If lenght of password inputted is less than 8 or greater than 128 exit the code.
       alert("You must choose alteast one of the following: lowercase, uppercase, numeric value, special character");
+      var retVal = "You must choose alteast one of the following: lowercase, uppercase, numeric value, special character";
+      return retVal;
     }
 
     //Declare empty string variable to containt the string:
     var retVal = "";
     
-    // Create a loop and run for the length selected by the user
-    // Initialized i to 0 and n to the length of the charSet
-    // Run loop till length is reached
-    // Add each value to retVal in each loop
+    /* Create a loop and run for the length selected by the user
+      Initialized i to 0 and n to the length of the charSet
+      Run loop till length is reached
+      Add each value to retVal in each loop */
     for(var i = 0, n = charSet.length; i < length; ++i) {
       retVal += charSet.charAt(Math.floor(Math.random() * n));
     }
@@ -52,6 +56,8 @@ function generatePassword(){
 
     // If lenght of password inputted is less than 8 or greater than 128 exit the code.
     window.alert("Length must be between 8 and 128");
+    var retVal = "Length must be between 8 and 128. Try Again";
+    return retVal;
   }
 
 }
