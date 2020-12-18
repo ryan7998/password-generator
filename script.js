5a3f5ff820eff8e6af559dab104ca5f85cc42dd0
fazle <ryan7998@gmail.com>
2020-12-17 19:54:44 -0500

    lower upper numeric function added

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
 // Assignment code here
+function generatePassword(){
+  var lowerCase, upperCase, numericValue;
+  var length = prompt("Length of password");
+  if(length >= 8 && length <= 128){
+    var charSet = "";
+    if(lowerCase = window.confirm("Do you  want the password to contain lowercase ")){
+      charSet = "abcdefghijklmnopqrstuvwxyz";
+    }
+    if(upperCase = window.confirm("Do you  want the password to contain UPPERCASE ")){
+      charSet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    }
+    if(numericValue = window.confirm("Do you  want the password to contain Numeric Value ")){
+      charSet += "0123456789";
+    }
 
+    console.log(charSet);
+    // debugger;
+    //var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    var retVal = "";
+    
+    for(var i = 0, n = charSet.length; i < length; ++i) {
+      retVal += charSet.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+    //  console.log(charset.charAt(1));
+  }else{
+    window.alert("Length must be between 8 and 128");
+  }
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
